0181eca4a79229d3787ede709c3653b5faa1983d
automatedtendencies <radler@thelinkbuilders.com>
2012-04-02 17:07:34 -0400

    MS Word shit with string detection

--- lib/to_csv-rails.rb
+++ lib/to_csv-rails.rb
@@ -1,26 +1,12 @@
 class Array
   
   def fix_stupid_quotes!(s) 
-    s.gsub!(/\x82/,',') 
-    s.gsub!(/\x84/,',,') 
-    s.gsub!(/\x85/,'...') 
-    s.gsub!(/\x88/,'^') 
-    s.gsub!(/\x89/,'o/oo') 
-    s.gsub!(/\x8b/,'<') 
-    s.gsub!(/\x8c/,'OE') 
-    s.gsub!(/\x91|\x92/,"'") 
-    s.gsub!(/\x93|\x94/,'"') 
-    s.gsub! "\342\200\230", "'"
-    s.gsub! "\342\200\231", "'"
-    s.gsub! "\342\200\234", '"'
-    s.gsub! "\342\200\235", '"'
-    s.gsub!(/\x95/,'*') 
-    s.gsub!(/\x96/,'-') 
-    s.gsub!(/\x97/,'--') 
-    s.gsub!(/\x98/,'~') 
-    s.gsub!(/\x99/,'TM') 
-    s.gsub!(/\x9b/,'>') 
-    s.gsub!(/\x9c/,'oe') 
+    return if s.class != String
+    s.gsub! "\u2019", "'"
+    s.gsub! "\u2018", "'"
+    s.gsub! "\u201C","\""
+    s.gsub! "\u201D","\"" 
+    return s
   end
 
   def to_csv(options = {})
@@ -42,9 +28,9 @@ class Array
     data << columns.map(&:to_s).map(&:humanize).join(',') if options[:header]
 
     self.each do |obj|
-      data << columns.map{ |column| obj.send(fix_stupid_quotes!(column.to_s)) }.join(',')
+      data << columns.map{ |column| fix_stupid_quotes!(obj.send(column)) }.join(',')
     end
-    data.join("\n")
+    data = data.join("\n")
   end
   
 end
