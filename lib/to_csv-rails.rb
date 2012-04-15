8bf493ee2567eaf728ceb74187be6fa9ad9d2090
automatedtendencies <radler@thelinkbuilders.com>
2012-04-15 13:11:30 -0400

    Cloned to csv_export() as well

--- lib/to_csv-rails.rb
+++ lib/to_csv-rails.rb
@@ -5,11 +5,35 @@ class Array
     s.gsub! "\u2019", "'"
     s.gsub! "\u2018", "'"
     s.gsub! "\u201C","\""
-    s.gsub! "\u201D","\"" 
+    s.gsub! "\u201D","\""
     return s
   end
 
   def to_csv(options = {})
+    return '' if self.empty?
+
+    options.reverse_merge!(:header => true)
+
+    #columns = self.first.class.content_columns # not include the ID column
+    if options[:only]
+      columns = Array(options[:only]).map(&:to_sym)
+    else
+      columns = self.first.class.column_names.map(&:to_sym) - Array(options[:except]).map(&:to_sym)
+    end
+    
+    return '' if columns.empty?
+    
+    data = []
+    # header
+    data << columns.map(&:to_s).map(&:humanize).join(',') if options[:header]
+
+    self.each do |obj|
+      data << columns.map{ |column| fix_stupid_quotes!(obj.send(column)) }.join(',')
+    end
+    data = data.join("\n")
+  end
+
+  def csv_export(options = {})
     return '' if self.empty?
 
     options.reverse_merge!(:header => true)
